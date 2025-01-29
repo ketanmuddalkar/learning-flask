093f85bca80c23b06dd2e5a4f39f9e8f8c91344b
Ketan Anil Muddalkar <ketanmuddalkar98@gmail.com>
2025-01-29 16:00:17 +0000

    Creating new routes

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,37 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return "Hello World!"
+
+
+@app.route("/success/<int:score>")
+def success(score: int):
+    return "The person has passed and the marks is " + str(score)
+
+
+@app.route("/fail/<int:score>")
+def fail(score: int):
+    return "The person has failed and the marks is " + str(score)
+
+
+@app.route("/results/<int:score>")
+def results(score: int):
+    """
+    This function is a score checker.
+
+    if score > 50:
+        send to success
+    else:
+        send to fail
+    """
+
+    result = ""
+
+    if score < 50:
+        result = "fail"
+    else:
+        result = "success"
+
+    return redirect(url_for(result, score=score))
 
 
 if __name__ == "__main__":
