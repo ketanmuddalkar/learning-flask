a95b0fa554634392113c22e8cf8bad50961fe5c0
Ketan Anil Muddalkar <ketanmuddalkar98@gmail.com>
2025-02-24 22:18:03 +0000

    added routing

--- src/app.py
+++ src/app.py
@@ -1,21 +1,21 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return "Hello World!"
+    return render_template("index.html")
 
 
 @app.route("/success/<int:score>")
 def success(score: int):
-    return "The person has passed and the marks is " + str(score)
+    return render_template("results.html")
 
 
 @app.route("/fail/<int:score>")
 def fail(score: int):
-    return "The person has failed and the marks is " + str(score)
+    return render_template("results.html")
 
 
 @app.route("/results/<int:score>")
@@ -37,6 +37,31 @@ def results(score: int):
         result = "success"
 
     return redirect(url_for(result, score=score))
+
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    """
+    This function will display html page for the results.
+    """
+
+    total_score = 0
+
+    if request.method == "POST":
+        science = float(request.form["science"])
+        maths = float(request.form["maths"])
+        c_prog = float(request.form["c"])
+        datascience = float(request.form["datascience"])
+
+        total_score = (science + maths + c_prog + datascience) / 4
+
+    res = ""
+    if total_score >= 50:
+        res = "success"
+    else:
+        res = "fail"
+
+    return redirect(url_for(res, score=total_score))
 
 
 if __name__ == "__main__":
